a25ff6c3c0212bb72d14b133c361ee5b697acdf5
Miqueias Mesquita <miqueias_jed@hotmail.com>
2025-10-06 17:39:26 -0300

    ajuste botao

--- resources/js/Pages/WorkOrders/Edit.vue
+++ resources/js/Pages/WorkOrders/Edit.vue
@@ -60,7 +60,7 @@
                     id="technician_id"
                     v-model="form.technician_id"
                     required
-                    class="flex-1 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-green-500"
+                    class="flex-1 h-10 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-green-500"
                     :class="{ 'border-red-500': form.errors.technician_id }"
                   >
                     <option value="">Selecione um técnico</option>
@@ -71,7 +71,7 @@
                   <button
                     type="button"
                     @click="showTechnicianModal = true"
-                    class="px-3 py-2 text-green-600 border border-green-300 rounded-md hover:bg-green-50 focus:outline-none focus:ring-2 focus:ring-green-500 transition-colors"
+                    class="h-10 w-10 text-green-600 border border-green-300 rounded-md hover:bg-green-50 focus:outline-none focus:ring-2 focus:ring-green-500 transition-colors flex items-center justify-center"
                     title="Adicionar novo técnico"
                   >
                     <svg class="h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
